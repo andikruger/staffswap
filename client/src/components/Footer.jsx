d876da50ac04a8f9810f3cda2c93a16fe895a5c5
Andreas Krueger <andreas_kruger@hotmail.com>
2024-03-06 07:53:30 +0100

    add privacy link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   // get the current year
   const year = new Date().getFullYear();
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {year} StaffSwap. All rights reserved.</p>
+        <p>
+          &copy; {year} StaffSwap. All rights reserved.{" "}
+          <Link to="/privacy" className="text-blue-300">
+            Privacy Policy
+          </Link>
+        </p>
       </div>
     </footer>
   );
 };
+
 export default Footer;
