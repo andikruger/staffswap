4f1ef34aba2e9ebd4a32b2186b49c3f2af7ca1ba
Andreas Krueger <andreas_kruger@hotmail.com>
2024-03-07 22:45:49 +0100

    handled swap not found

--- client/src/pages/swap/swap.jsx
+++ client/src/pages/swap/swap.jsx
@@ -202,142 +202,153 @@ const SwapDetails = () => {
         }}
       >
         {/* White rounded box */}
-        <div className="bg-white p-8 my-4 rounded-lg shadow-lg w-11/12 max-w-screen-md overflow-y-auto">
-          {/* Your content goes here */}
-          <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center text-center">
-            Swap Details
-          </h2>
+        {swapDetails ? (
+          <div className="bg-white p-8 my-4 rounded-lg shadow-lg w-11/12 max-w-screen-md overflow-y-auto">
+            {/* Your content goes here */}
+            <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center text-center">
+              Swap Details
+            </h2>
 
-          {/* Name and Three Letter Code in a column for smaller screens */}
-          <div className="pt-4 flex flex-col sm:flex-row mb-4">
-            {/* Name */}
-            <div className="w-full sm:w-1/2 mb-2 sm:mb-0 sm:mr-2">
-              <label htmlFor="name" className="block text-sm mb-2 text-center">
-                Name
-              </label>
-              <p
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
-                id="name"
-              >
-                {swapDetails.name}
-              </p>
+            {/* Name and Three Letter Code in a column for smaller screens */}
+            <div className="pt-4 flex flex-col sm:flex-row mb-4">
+              {/* Name */}
+              <div className="w-full sm:w-1/2 mb-2 sm:mb-0 sm:mr-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm mb-2 text-center"
+                >
+                  Name
+                </label>
+                <p
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
+                  id="name"
+                >
+                  {swapDetails.name}
+                </p>
+              </div>
+
+              {/* Three Letter Code */}
+              <div className="w-full sm:w-1/2 ml-0 sm:ml-2">
+                <label
+                  htmlFor="threeLetterCode"
+                  className="block text-sm mb-2 text-center"
+                >
+                  Three Letter Code
+                </label>
+                <p
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
+                  id="threeLetterCode"
+                >
+                  {swapDetails.threeLetterCode}
+                </p>
+              </div>
             </div>
 
-            {/* Three Letter Code */}
-            <div className="w-full sm:w-1/2 ml-0 sm:ml-2">
+            {/* Date and Shift Time in a column for smaller screens */}
+            <div className="flex flex-col sm:flex-row mb-4">
+              {/* Date */}
+              <div className="w-full sm:w-1/2 mb-2 sm:mb-0 sm:mr-2">
+                <label
+                  htmlFor="date"
+                  className="block text-sm mb-2 text-center"
+                >
+                  Date
+                </label>
+                <p
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
+                  id="date"
+                >
+                  {formatDate(swapDetails.date)}
+                </p>
+              </div>
+
+              {/* Shift Time */}
+              <div className="w-full sm:w-1/2 ml-0 sm:ml-2">
+                <label
+                  htmlFor="shiftTime"
+                  className="block text-sm mb-2 text-center"
+                >
+                  Shift Time
+                </label>
+                <p className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center">
+                  {swapDetails.startTime} - {swapDetails.endTime} (
+                  {swapDetails.duration} hours)
+                </p>
+              </div>
+            </div>
+
+            <div className="mb-4">
               <label
-                htmlFor="threeLetterCode"
+                htmlFor="priority"
                 className="block text-sm mb-2 text-center"
               >
-                Three Letter Code
-              </label>
-              <p
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
-                id="threeLetterCode"
-              >
-                {swapDetails.threeLetterCode}
-              </p>
-            </div>
-          </div>
-
-          {/* Date and Shift Time in a column for smaller screens */}
-          <div className="flex flex-col sm:flex-row mb-4">
-            {/* Date */}
-            <div className="w-full sm:w-1/2 mb-2 sm:mb-0 sm:mr-2">
-              <label htmlFor="date" className="block text-sm mb-2 text-center">
-                Date
-              </label>
-              <p
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center"
-                id="date"
-              >
-                {formatDate(swapDetails.date)}
-              </p>
-            </div>
-
-            {/* Shift Time */}
-            <div className="w-full sm:w-1/2 ml-0 sm:ml-2">
-              <label
-                htmlFor="shiftTime"
-                className="block text-sm mb-2 text-center"
-              >
-                Shift Time
+                Priority
               </label>
               <p className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center">
-                {swapDetails.startTime} - {swapDetails.endTime} (
-                {swapDetails.duration} hours)
+                {formatPriority(swapDetails.priority)}
               </p>
             </div>
-          </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="priority"
-              className="block text-sm mb-2 text-center"
-            >
-              Priority
-            </label>
-            <p className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center">
-              {formatPriority(swapDetails.priority)}
-            </p>
-          </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2 text-center">
+                Shift Wish
+              </label>
+              <div>
+                {/* ... Radio buttons ... */}
+                <ShiftWishList
+                  options={shiftWish}
+                  selectedOption={selectedOption}
+                />
+              </div>
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-sm mb-2 text-center">Shift Wish</label>
-            <div>
-              {/* ... Radio buttons ... */}
-              <ShiftWishList
-                options={shiftWish}
-                selectedOption={selectedOption}
+            <div className="mb-4">
+              <label htmlFor="note" className="block text-sm mb-2 text-center">
+                Note
+              </label>
+              <p className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center">
+                {swapDetails.note || "No note provided"}
+              </p>
+            </div>
+
+            {/* Exchanges */}
+
+            <div className="mb-4">
+              <label className="block text-sm mb-2 text-center">
+                Exchanges
+              </label>
+              <ul>
+                {Array.isArray(swapDetails.exchanges) &&
+                  swapDetails.exchanges.map((exchange, index) => (
+                    <li
+                      key={index}
+                      className="text-center"
+                      style={{
+                        backgroundColor: "#e0211a",
+                        borderRadius: "20px",
+                        padding: "10px 20px",
+                        margin: "8px",
+                        color: "white",
+                        border: "none", // Add border for unchecked items
+                      }}
+                    >
+                      {`Date: ${formatDate(exchange.date)}, ${
+                        exchange.startTime
+                      } - ${exchange.endTime}`}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2">
+                Qualifications Required
+              </label>
+              <QualificationsList
+                options={qualifications}
+                selectedOptions={selectedOptions}
               />
             </div>
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="note" className="block text-sm mb-2 text-center">
-              Note
-            </label>
-            <p className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-center">
-              {swapDetails.note || "No note provided"}
-            </p>
-          </div>
-
-          {/* Exchanges */}
-
-          <div className="mb-4">
-            <label className="block text-sm mb-2 text-center">Exchanges</label>
-            <ul>
-              {Array.isArray(swapDetails.exchanges) &&
-                swapDetails.exchanges.map((exchange, index) => (
-                  <li
-                    key={index}
-                    className="text-center"
-                    style={{
-                      backgroundColor: "#e0211a",
-                      borderRadius: "20px",
-                      padding: "10px 20px",
-                      margin: "8px",
-                      color: "white",
-                      border: "none", // Add border for unchecked items
-                    }}
-                  >
-                    {`Date: ${formatDate(exchange.date)}, ${
-                      exchange.startTime
-                    } - ${exchange.endTime}`}
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm mb-2">
-              Qualifications Required
-            </label>
-            <QualificationsList
-              options={qualifications}
-              selectedOptions={selectedOptions}
-            />
-          </div>
-          {/* <div className="mb-4">
+            {/* <div className="mb-4">
             <label htmlFor="note" className="block text-sm mb-2 text-center">
               Email
             </label>
@@ -345,7 +356,7 @@ const SwapDetails = () => {
               {email || "No email provided"}
             </p>
           </div> */}
-          {/* 
+            {/* 
           <div className="mb-4">
             <label htmlFor="note" className="block text-sm mb-2">
               Phone Number
@@ -354,29 +365,34 @@ const SwapDetails = () => {
               {phone || "No Phone Number provided"}
             </p>
           </div> */}
-          <div className="items-center justify-center content-center">
-            {/* Display buttons if userID = swap.userID */}
-            {userID === swapDetails.userID && (
-              <div className="flex flex-col sm:flex-row justify-between">
-                <DeleteSwapButton id={id} />
-                <MatchButton id={id} swapDetails={swapDetails} />
-                <EditSwapButton id={id} />
-              </div>
-            )}
+            <div className="items-center justify-center content-center">
+              {/* Display buttons if userID = swap.userID */}
+              {userID === swapDetails.userID && (
+                <div className="flex flex-col sm:flex-row justify-between">
+                  <DeleteSwapButton id={id} />
+                  <MatchButton id={id} swapDetails={swapDetails} />
+                  <EditSwapButton id={id} />
+                </div>
+              )}
 
-            {/* Display match button if userID != swap.userID */}
-            {userID !== swapDetails.userID && (
-              <div className="flex flex-col sm:flex-row justify-between">
-                <ChatButton
-                  creatorId={userID}
-                  partnerId={swapDetails.userID}
-                  swapDetails={swapDetails}
-                />
-                <MatchButton id={id} swapDetails={swapDetails} />
-              </div>
-            )}
+              {/* Display match button if userID != swap.userID */}
+              {userID !== swapDetails.userID && (
+                <div className="flex flex-col sm:flex-row justify-between">
+                  <ChatButton
+                    creatorId={userID}
+                    partnerId={swapDetails.userID}
+                    swapDetails={swapDetails}
+                  />
+                  <MatchButton id={id} swapDetails={swapDetails} />
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        ) : (
+          <p className="bg-white p-8 rounded-lg shadow-lg m-4 max-w-md">
+            Swap not found
+          </p>
+        )}
       </div>
 
       <Footer />
