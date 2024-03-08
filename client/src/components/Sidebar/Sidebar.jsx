d44607dbd5166f05b0c99868c579fb4af6c0cb69
Andreas Krueger <andreas_kruger@hotmail.com>
2024-03-08 07:46:26 +0100

    hopefully chat bug fixed

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import UserChats from "./UserChats";
 import UserPanel from "./UserPanel";
 import { ToastContainer } from "react-toastify";
+
 function Sidebar() {
+  const [firstVisit, setFirstVisit] = useState(false);
+
+  useEffect(() => {
+    const hasVisitedBefore = sessionStorage.getItem("hasVisitedSidebar");
+    if (!hasVisitedBefore) {
+      setFirstVisit(true);
+      sessionStorage.setItem("hasVisitedSidebar", "true");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (firstVisit) {
+      window.location.reload();
+    }
+  }, [firstVisit]);
+
   return (
     <div className="flex flex-col w-80 border-r border-divider h-screen">
       <ToastContainer draggable closeOnClick />
